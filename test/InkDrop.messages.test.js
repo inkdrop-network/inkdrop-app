ae2f0b84bb2655a85d9c9a10885299f33df6392f
mikas <mi.kaserer@gmail.com>
2018-05-18 21:13:40 +0200

    Unlike extended test added.

--- test/InkDrop.messages.test.js
+++ test/InkDrop.messages.test.js
@@ -3,6 +3,9 @@ const InkDrop = artifacts.require('InkDrop')
 contract('InkDrop (message functions)', async accounts => {
   before('initial setup with user creation', async () => {
     let inkdropInstance = await InkDrop.deployed()
+    await inkdropInstance.createUser(accounts[7], 'testuser7', 'testbio', 'testhash', {
+      from: accounts[7],
+    })
     await inkdropInstance.createUser(accounts[8], 'testuser8', 'testbio', 'testhash', {
       from: accounts[8],
     })
@@ -114,9 +117,9 @@ contract('InkDrop (message functions)', async accounts => {
     let msg = await inkdropInstance.getMessage(0, { from: accounts[9] })
     assert.equal(msg[4].length, 2, 'The message should have 2 like.')
     await inkdropInstance.unlikeMessage(0, { from: accounts[9] })
-    // msg = await inkdropInstance.getMessage(0, { from: accounts[9] })
-    // assert.equal(msg[4].length, 1, 'The message should now have 1 likes.')
-    // assert.equal(msg[4][0], accounts[8], 'The user address should be stored.')
+    msg = await inkdropInstance.getMessage(0, { from: accounts[9] })
+    assert.equal(msg[4].length, 1, 'The message should now have 1 likes.')
+    assert.equal(msg[4][0], accounts[8], 'The user address should be stored.')
   })
 
   it('...unlike message twice', async () => {
@@ -139,5 +142,30 @@ contract('InkDrop (message functions)', async accounts => {
       const revertFound = error.message.search('revert') >= 0
       assert.equal(revertFound, true, `Expected "revert", got ${error} instead`)
     }
+  })
+
+  it('...unlike message extended', async () => {
+    let inkdropInstance = await InkDrop.deployed()
+    let msg = await inkdropInstance.getMessage(0, { from: accounts[9] })
+    assert.equal(msg[4].length, 1, 'The message should have 1 like.')
+    await inkdropInstance.likeMessage(0, { from: accounts[9] })
+    await inkdropInstance.likeMessage(0, { from: accounts[7] })
+
+    msg = await inkdropInstance.getMessage(0, { from: accounts[9] })
+    assert.equal(msg[4].length, 3, 'The message should have 3 like.')
+    assert.equal(msg[4][0], accounts[8], 'The user address should be stored.')
+    assert.equal(msg[4][1], accounts[9], 'The user address should be stored.')
+    assert.equal(msg[4][2], accounts[7], 'The user address should be stored.')
+
+    await inkdropInstance.unlikeMessage(0, { from: accounts[9] })
+    msg = await inkdropInstance.getMessage(0, { from: accounts[9] })
+    assert.equal(msg[4].length, 2, 'The message should now have 2 likes.')
+    assert.equal(msg[4][0], accounts[8], 'The user address should be stored.')
+    assert.equal(msg[4][1], accounts[7], 'The user address should be stored.')
+
+    await inkdropInstance.unlikeMessage(0, { from: accounts[8] })
+    msg = await inkdropInstance.getMessage(0, { from: accounts[8] })
+    assert.equal(msg[4].length, 1, 'The message should now have 1 likes.')
+    assert.equal(msg[4][0], accounts[7], 'The user address should be stored.')
   })
 })
