41af1ddac1db96d85dd6b9e5edef473c0dad2388
mikas <mi.kaserer@gmail.com>
2018-05-28 21:25:35 +0200

    truffle.js reset

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,5 @@ var InkDrop = artifacts.require('InkDrop')
 
 module.exports = function(deployer) {
 	deployer.deploy(Authentication)
-	// deployer.deploy(Ownable)
-	// deployer.link(Ownable, InkDrop)
 	deployer.deploy(InkDrop)
 }
---
+++ truffle.js
@@ -0,0 +1,42 @@
+var HDWalletProvider = require('truffle-hdwallet-provider')
+const MNEMONIC = process.env.MNEMONIC
+const INFURA_API_KEY = process.env.INFURA_API_KEY
+
+module.exports = {
+	// See <http://truffleframework.com/docs/advanced/configuration>
+	// to customize your Truffle configuration!
+	networks: {
+		development: {
+			host: '127.0.0.1',
+			port: 8545,
+			// provider: function() {
+			// 	return new HDWalletProvider(MNEMONIC, 'http://127.0.0.1:8545/')
+			// },
+			network_id: '*', // Match any network id
+		},
+		ropsten: {
+			provider: function() {
+				return new HDWalletProvider(MNEMONIC, 'https://ropsten.infura.io/' + INFURA_API_KEY)
+			},
+			network_id: 3,
+			gas: 4712388,
+			gasPrice: 100000000000,
+		},
+		kovan: {
+			provider: function() {
+				return new HDWalletProvider(MNEMONIC, 'https://kovan.infura.io/' + INFURA_API_KEY)
+			},
+			network_id: 42,
+			gas: 4712388,
+			gasPrice: 100000000000,
+		},
+		rinkeby: {
+			provider: function() {
+				return new HDWalletProvider(MNEMONIC, 'https://rinkeby.infura.io/' + INFURA_API_KEY)
+			},
+			network_id: 4,
+			gas: 4712388,
+			gasPrice: 100000000000,
+		},
+	},
+}
