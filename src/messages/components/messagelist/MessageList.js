73530b51775ed79129602613c3fb01e54bc11c62
mikas <mi.kaserer@gmail.com>
2018-09-06 19:11:24 +0200

    Cleanup.

--- src/messages/components/messagelist/MessageList.js
+++ src/messages/components/messagelist/MessageList.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { CardColumns, Button } from 'reactstrap'
 import InfiniteScroll from 'react-infinite-scroller'
 import FlipMove from 'react-flip-move'
 // import MasonryInfiniteScroller from 'react-masonry-infinite'
@@ -24,12 +23,12 @@ class MessageList extends PureComponent {
   }
 
   loadMore() {
-    // this.props.fetchMessages(this.props.pagination.items)
-  }
-
-  componentDidMount() {
     this.props.fetchMessages(this.props.pagination.items)
   }
+
+  // componentDidMount() {
+  //   this.props.fetchMessages(this.props.pagination.items)
+  // }
 
   sortNewsfeed() {
     this.props.sortNewsfeed()
@@ -42,16 +41,11 @@ class MessageList extends PureComponent {
       </div>
     )
 
-    let items = this.props.messages
+    // let items = this.props.messages
     // sort messages descending according to their drops
-    // let items = this.props.messages.sort(function(msgA, msgB) {
-    //   return msgB.drops - msgA.drops
-    // })
-    // let childElements = this.props.messages.map(msg => (
-    //   <div className="grid-item col-3" key={msg.id}>
-    //     {msg.content}
-    //   </div>
-    // ))
+    let items = this.props.messages.sort(function(msgA, msgB) {
+      return msgB.drops - msgA.drops
+    })
 
     // TODO: Show loader in the beginning and enable FlipMove only once the first posts are fetched and the array is sorted
     return (
@@ -62,38 +56,16 @@ class MessageList extends PureComponent {
         loadMore={this.loadMore}
         hasMore={this.props.pagination.hasMore}
         loader={loader}>
-        <Button size="sm" color="secondary" className="mb-4 ml-auto" onClick={this.sortNewsfeed}>
-          Sort
-        </Button>
-        <CardColumns>
-          <FlipMove
-            disableAllAnimations={true} //this.props.messages.length <= 0}
-            appearAnimation="none"
-            enterAnimation="none"
-            leaveAnimation="none"
-            duration={750}
-            staggerDurationBy={30}>
-            {items.map(msg => <MessageItemContainer message={msg} key={msg.id} />)}
-          </FlipMove>
-        </CardColumns>
+        <FlipMove
+          disableAllAnimations={false} //this.props.messages.length <= 0}
+          appearAnimation="none"
+          enterAnimation="none"
+          leaveAnimation="none"
+          duration={0}
+          staggerDurationBy={0}>
+          {items.map(msg => <MessageItemContainer message={msg} key={msg.id} />)}
+        </FlipMove>
       </InfiniteScroll>
-      /*<MasonryInfiniteScroller
-        className="messages"
-        sizes={this.props.sizes}
-        position={false}
-        initialLoad={false}
-        loadMore={() => console.log(1)}
-        hasMore={false}
-        loader={loader}>
-        {items.map(msg => <MessageItemContainer message={msg} key={msg.id} />)}
-      </MasonryInfiniteScroller>*/
-      /*<Masonry className="grid" options={this.masonryOptions}>
-        <div className="grid-sizer col-3" />
-        {childElements}
-      </Masonry>*/
-      /*<CardColumns>
-        {items.map(msg => <MessageItemContainer message={msg} key={msg.id} />)}
-      </CardColumns>*/
     )
   }
 }
--- src/messages/layouts/Newsfeed.js
+++ src/messages/layouts/Newsfeed.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react'
 import MessageListMasonryContainer from '../components/messagelist/MessageListMasonryContainer'
 
 class Newsfeed extends Component {
-  constructor(props, { authData }) {
-    super(props)
-    authData = this.props
-  }
-
   render() {
     return <MessageListMasonryContainer />
   }
