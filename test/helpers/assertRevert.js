57c6fd0366d8d606b2d70713634dd3ef9737b6df
mikas <mi.kaserer@gmail.com>
2018-09-07 22:16:00 +0200

    Test helpers removed 🙅‍♂️.

--- test/helpers/assertRevert.js
+++
@@ -1,9 +0,0 @@
-export default async promise => {
-	try {
-		await promise
-		assert.fail('Should throw error.')
-	} catch (error) {
-		const revertFound = error.message.search('revert') >= 0
-		assert(revertFound, `Expected "revert", got ${error} instead`)
-	}
-}
