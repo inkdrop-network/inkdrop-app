4d9d6d17d4c3c81d80ba85ce28c3d95775cf58b0
mikas <mi.kaserer@gmail.com>
2018-09-07 22:21:11 +0200

    Proxy tests started [WIP].

--- test/InkDrop.proxy.test.js
+++ test/InkDrop.proxy.test.js
@@ -1,25 +1,10 @@
-// import { TestApp } from 'zos'
 let TestApp = require('../node_modules/zos').TestApp
 
 const InkDrop = artifacts.require('InkDrop')
 
-contract('InkDrop proxy pattern', function([_, owner, recipient]) {
-	beforeEach(async function() {
+contract('InkDrop reduceDrops', ([_, owner, ...accounts]) => {
+	before(async () => {
 		this.app = await TestApp() //{ from: owner }, 'zos.development.json')
+		this.inkdropInstance = await this.app.createProxy(InkDrop, 'InkDrop', 'initialize', [owner])
 	})
-
-	describe('via proxy', function() {
-		it('should create a proxy', async function() {
-			// Check description here: https://github.com/zeppelinos/zos/issues/60#issuecomment-419140273
-			const proxy = await this.app.createProxy(InkDrop, 'InkDrop', 'initialize', [owner])
-		})
-	})
-
-	// describe('via proxy, total supply', function() {
-	// 	it('should create a proxy for the stdlib', async function() {
-	// 		const proxy = await this.app.createProxy(InkDrop, 'InkDrop', 'initialize') //, { owner: owner })
-	// 		// const result = await proxy.totalSupply()
-	// 		// assert.equal(result.toNumber(), 0, 'Should have 0 supply.')
-	// 	})
-	// })
 })
